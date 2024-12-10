fb717317cae35f9e4adb19d0eedd7ccf9278c099
ferlloher <ferlloher@edu.gva.es>
2024-12-10 14:44:26 +0100

    Creado archivo TAreas.java con una suma

---
+++ Tareas.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Tareas{
+
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" Seleccione el primer numero");
+
+
+        int x=sc.nextInt();
+
+        System.out.println(" Seleccione el segundo numero");
+
+        int b=sc.nextInt();
+
+        int suma=x+b;
+
+        System.out.println("El resultado de la suma es: "+suma);
+
+
+    }
+        
+
+    
+
+}
